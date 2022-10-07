fcec78de461efcb39e12d06f9166e8d21b30b5fc
Pélé Oussoumanou <43408405+othimar@users.noreply.github.com>
2022-10-07 15:21:29 +0100

    Update in-work.jsx

--- pages/in-work.jsx
+++ pages/in-work.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 export default function InWork(){
     return(
         <Layout>
-            <h1 style={{'text-align':'center', margin:'150px 0px'}}>Currently In work</h1>
+            <h1 style={{'text-align':'center', margin:'150px 0px'}}>En travaux...</h1>
         </Layout>
     );
 }
